3319fae41527cce821eb5264bfb6fc3198089753
trevodorax <phvcgdx@mail.com>
2023-04-30 08:43:12 +0200

    18: add main logo as SVG

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,11 +1,15 @@
 import Navigation from '@/components/navigation/Navigation'
+import { MainLogo } from '../svgs/MainLogo'
 
 import styles from './Header.module.scss'
 
 export default function Header () {
   return (
     <header className={styles.container}>
-      <h1>Cook Masters</h1>
+      <div className={styles.mainLogo}>
+        <MainLogo />
+      </div>
+      <h1>Cook Master</h1>
       <Navigation />
     </header>
   )
