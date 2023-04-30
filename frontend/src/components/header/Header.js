c3fd5cd83851d757f45238f7a68bfa459a29b7be
trevodorax <phvcgdx@mail.com>
2023-04-30 09:46:05 +0200

    18: make header fit design + create burger menu skeleton

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,16 +1,39 @@
+import { useState } from 'react'
+import cx from 'classnames'
+
 import Navigation from '@/components/navigation/Navigation'
 import { MainLogo } from '../svgs/MainLogo'
 
 import styles from './Header.module.scss'
+import { BurgerIcon } from '../svgs/BurgerIcon'
 
 export default function Header () {
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false)
+
+  const toggleBurgerMenu = () => {
+    setIsBurgerMenuOpen((isOpen) => !isOpen)
+  }
+
   return (
     <header className={styles.container}>
       <div className={styles.mainLogo}>
         <MainLogo />
       </div>
-      <h1>Cook Master</h1>
-      <Navigation />
+      <h1 className={styles.title}>Cook Master</h1>
+      <div
+        className={cx(styles.burgerIcon, {
+          [styles.open]: isBurgerMenuOpen
+        })}
+        onClick={toggleBurgerMenu}
+      >
+        <BurgerIcon />
+      </div>
+      <Navigation
+        containerClassname={cx(styles.navigationContainer, {
+          [styles.open]: isBurgerMenuOpen
+        })}
+        itemsClassname={styles.navigationItem}
+      />
     </header>
   )
 }
---
+++ frontend/src/components/svgs/BurgerIcon.js
@@ -0,0 +1,8 @@
+export const BurgerIcon = () => (
+  <svg width='26' height='21' viewBox='0 0 26 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <path d='M1 1.5H25' stroke='currentColor' stroke-width='2' stroke-linecap='round' />
+    <path d='M1 10.5H25' stroke='currentColor' stroke-width='2' stroke-linecap='round' />
+    <path d='M1 19.5H25' stroke='currentColor' stroke-width='2' stroke-linecap='round' />
+  </svg>
+
+)
