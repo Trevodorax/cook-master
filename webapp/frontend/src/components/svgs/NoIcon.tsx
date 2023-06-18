3cea5b112c21a16f197fad05620544c74d29adf9
trevodorax <phvcgdx@mail.com>
2023-06-18 11:56:23 +0200

    45: setup i18n

--- webapp/frontend/src/components/svgs/NoIcon.tsx
+++ webapp/frontend/src/components/svgs/NoIcon.tsx
@@ -1,13 +1,13 @@
 export const NoIcon = () => (
   <svg width="81" height="81" viewBox="0 0 81 81" fill="none">
-    <circle cx="40.5" cy="40.5" r="36.5" stroke="#DD0000" stroke-width="8" />
+    <circle cx="40.5" cy="40.5" r="36.5" stroke="#DD0000" strokeWidth="8" />
     <line
       x1="24.8284"
       y1="25.1716"
       x2="56.6482"
       y2="56.9914"
       stroke="#DD0000"
-      stroke-width="8"
+      strokeWidth="8"
     />
     <line
       x1="24.1716"
@@ -15,7 +15,7 @@ export const NoIcon = () => (
       x2="55.9914"
       y2="26.1716"
       stroke="#DD0000"
-      stroke-width="8"
+      strokeWidth="8"
     />
   </svg>
 );
--- webapp/frontend/src/pages/_app.tsx
+++ webapp/frontend/src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-
+import "@/locales/i18n";
 import { RedirectionWrapper } from "@/store/redirection/RedirectionWrapper";
 import { store } from "@/store/store";
 import Layout from "@/components/layout/Layout";
 import { LocalStorageProvider } from "@/wrappers/LocalStorageProvider";
 import "@/styles/globals.scss";
+import { I18nextProvider } from "react-i18next";
+import i18n from "@/locales/i18n";
 
 const stripePromise = loadStripe(
   "pk_test_51NJeJKBzPmJlbME74bi9K2j3yJ2Txedv3tiNZE2MJdXcyNF01dpkDBMLwwB1s4BpNWeqJ8J8PdwuIsOMkwudJRmE00BXNPOzIc"
@@ -22,16 +24,18 @@ interface Props {
 export default function Index({ Component, pageProps }: Props) {
   return (
     <Provider store={store}>
-      <LocalStorageProvider>
-        <RedirectionWrapper>
-          <Elements stripe={stripePromise}>
-            <Layout>
-              <Toaster />
-              <Component {...pageProps} />
-            </Layout>
-          </Elements>
-        </RedirectionWrapper>
-      </LocalStorageProvider>
+      <I18nextProvider i18n={i18n}>
+        <LocalStorageProvider>
+          <RedirectionWrapper>
+            <Elements stripe={stripePromise}>
+              <Layout>
+                <Toaster />
+                <Component {...pageProps} />
+              </Layout>
+            </Elements>
+          </RedirectionWrapper>
+        </LocalStorageProvider>
+      </I18nextProvider>
     </Provider>
   );
 }
