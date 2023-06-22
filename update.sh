53bd909cd5d0e1520a95e3584e6cf7a696f14e0a
Paul Gaudeaux <paul.gaudeaux@sciforma.com>
2023-06-22 21:57:39 +0200

    test 6 for CD

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# cd /home/debian/cook-master
-# git pull --rebase
-# docker compose up -d && docker compose exec api npx prisma migrate dev
-touch /home/deploy/test.txt
-echo "PONEY" > /home/deploy/test.txt
+cd /home/debian/cook-master
+git pull --rebase
+docker compose up -d && docker compose exec api npx prisma migrate dev --preview-feature
