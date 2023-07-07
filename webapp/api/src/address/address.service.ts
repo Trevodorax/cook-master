37fd3373e4d875eee9b349dfb238426408a771fd
trevodorax <phvcgdx@mail.com>
2023-07-08 00:03:22 +0200

    73: util function to get coords

--- webapp/api/src/address/address.service.ts
+++ webapp/api/src/address/address.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { CreateAddressDto } from 'src/premise/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AddressService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private configService: ConfigService,
+  ) {}
 
   async createAddress(address: CreateAddressDto) {
+    const coords = await this.getCoordsFromAddress(
+      address.streetNumber,
+      address.streetName,
+      address.postalCode,
+      address.city,
+      address.country,
+      1,
+    );
+
+    console.log(coords);
+
     const newAddress = await this.prisma.address.create({
       data: {
         streetName: address.streetName,
@@ -19,5 +34,43 @@ export class AddressService {
     });
 
     return newAddress;
+  }
+
+  async getCoordsFromAddress(
+    houseNumber: string,
+    streetName: string,
+    postCode: string,
+    city: string,
+    country: string,
+    nbResultsMax: number,
+  ): Promise<{ lon: string; lat: string; confidence: number }> {
+    const geocodingAPIKey = this.configService.get('GEOCODING_API_KEY');
+
+    const rawResponse = await fetch(
+      `https://api.geoapify.com/v1/geocode/search?housenumber=${encodeURIComponent(
+        houseNumber,
+      )}&street=${encodeURIComponent(streetName)}
+      &postcode=${encodeURIComponent(postCode)}&city=${encodeURIComponent(
+        city,
+      )}&country=${encodeURIComponent(
+        country,
+      )}&limit=${nbResultsMax}&format=json&apiKey=${geocodingAPIKey}`,
+    );
+
+    try {
+      const response = await rawResponse.json();
+
+      if (!response?.results[0]) {
+        return null;
+      }
+
+      return {
+        lon: response.results[0].lon,
+        lat: response.results[0].lat,
+        confidence: response.results[0].rank.confidence,
+      };
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
