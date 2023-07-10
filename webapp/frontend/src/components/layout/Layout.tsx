8f68975f848a302ed0f6305fbd71b97abdb5c330
trevodorax <phvcgdx@mail.com>
2023-07-10 20:10:12 +0200

    74: forgotton polishes

--- webapp/frontend/src/components/layout/Layout.tsx
+++ webapp/frontend/src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode, useState } from "react";
 import Header from "@/components/header/Header";
 import styles from "./Layout.module.scss";
+import Head from "next/head";
 
 interface Props {
   children: ReactNode;
@@ -11,6 +12,19 @@ export default function Layout({ children }: Props) {
 
   return (
     <div id="root" className={styles.container}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+        <link
+          rel="icon"
+          href="/icon.svg"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <title>Cook Master</title>
+      </Head>
       <Header
         isNavigationOpen={isNavigationOpen}
         setIsNavigationOpen={setIsNavigationOpen}
